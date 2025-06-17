b3660901571e0753f822044dd87d5cd10939d642
Rounak858 <91734046+Rounak858@users.noreply.github.com>
2025-06-17 22:36:48 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Fleck;
+
+var server = new WebSocketServer("ws://0.0.0.0:5050");
+var allSockets = new List<IWebSocketConnection>();
+
+server.Start(socket =>
+{
+    socket.OnOpen = () =>
+    {
+        Console.WriteLine("Client connected");
+        allSockets.Add(socket);
+    };
+
+    socket.OnClose = () =>
+    {
+        Console.WriteLine("Client disconnected");
+        allSockets.Remove(socket);
+    };
+
+    socket.OnMessage = message =>
+    {
+        Console.WriteLine("Received: " + message);
+        foreach (var s in allSockets)
+        {
+            s.Send(message);
+        }
+    };
+});
+
+Console.WriteLine("WebSocket Server started on ws://0.0.0.0:5050");
+Task.Delay(-1).Wait();
+Console.ReadLine();
